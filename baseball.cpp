f38d878467715918c756c6795a7ec39d06d219f6
YoungJae Ryu <zeroexit@gmail.com>
2025-07-02 13:18:05 +0900

    [feature] p.27

--- baseball.cpp
+++ baseball.cpp
@@ -1,4 +1,10 @@
+#include <stdexcept>
+
+using namespace std;
+
 class Baseball {
 public:
-
+	void guess(const string& string) {
+		throw length_error("Must be three letters.");
+	}
 };
--- baseball_test.cpp
+++ baseball_test.cpp
@@ -1,8 +1,9 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
-	EXPECT_EQ(1, 1);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
 
 int main() {
