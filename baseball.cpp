3e409d06a9126b58270b9d47980001bb62280f44
YoungJae Ryu <zeroexit@gmail.com>
2025-07-02 13:19:07 +0900

    [refactoring p.28

--- baseball.cpp
+++ baseball.cpp
@@ -5,6 +5,8 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& string) {
-		throw length_error("Must be three letters.");
+		if (string.length() != 3) {
+			throw length_error("Must be three letters.");
+		}
 	}
 };
