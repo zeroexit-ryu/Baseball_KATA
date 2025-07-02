e6be15ffb0403625869e54b8d50d1d8325f805e7
YoungJae Ryu <zeroexit@gmail.com>
2025-07-02 13:21:27 +0900

    [feature] p.30

--- baseball.cpp
+++ baseball.cpp
@@ -8,5 +8,11 @@ public:
 		if (string.length() != 3) {
 			throw length_error("Must be three letters.");
 		}
+
+		for (char ch : string) {
+			if (ch < '0' || ch > '9') {
+				throw invalid_argument("Must be number");
+			}
+		}
 	}
 };
--- baseball_test.cpp
+++ baseball_test.cpp
@@ -6,6 +6,11 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
 
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
